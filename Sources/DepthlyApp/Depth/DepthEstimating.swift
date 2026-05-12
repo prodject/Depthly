d6b9c829af88eb9353500ecd5986a93974000214
Alexey Lazarev <anonymous.prodject@gmail.com>
2026-05-12 10:29:26 +0300

    Cleaned project

--- Sources/DepthlyApp/Depth/DepthEstimating.swift
+++
@@ -1,9 +0,0 @@
-import AVFoundation
-import CoreImage
-import CoreImage.CIFilterBuiltins
-
-protocol ForegroundMaskEstimating: Sendable {
-    func estimateForegroundMask(for pixelBuffer: CVPixelBuffer) async throws -> CIImage
-}
-
-typealias DepthEstimating = ForegroundMaskEstimating
